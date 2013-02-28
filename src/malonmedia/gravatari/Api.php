35d57f773710e0ac217a5679e676023e4fc2a4a6
Jason Walton <jwalton512@gmail.com>
2013-02-27 19:46:41 -0700

    moved setting of urlGenerator to constructor of Api base class

--- src/malonmedia/gravatari/Api.php
+++ src/malonmedia/gravatari/Api.php
@@ -1,6 +1,7 @@
 <?php namespace Gravatari;
 
 use Illuminate\Support\Fluent;
+use Gravatari\UrlGenerator;
 
 class Api {
 
@@ -12,13 +13,31 @@ class Api {
     protected $container;
 
     /**
+     * The url generator instance
+     *
+     * @var Gravatari\UrlGenerator
+     */
+    protected $urlGenerator;
+
+    /**
+     * Template for generating url
+     *
+     * @var string
+     */
+    protected $urlTemplate = "www.gravatar.com/%s%s";
+
+    /**
      * Constructor
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(UrlGenerator $urlGenerator = null)
     {
         $this->container = new Fluent;
+
+        $this->urlGenerator = $urlGenerator ?: new UrlGenerator;
+
+        $this->urlGenerator->setTemplate($this->urlTemplate);
     }
 
     /**
